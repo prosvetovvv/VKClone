e3567ea40f67032dc8dd1db83bc48e4690e778ba
prosvetovvv <vp12@yandex.ru>
2020-08-04 13:53:12 +0300

    Lesson2 Navigation VKClone.

--- VKClone/LoginFormController.swift
+++ VKClone/LoginFormController.swift
@@ -33,17 +33,17 @@ class LoginFormController: UIViewController {
     
     @IBAction func signUpPressed(_ sender: UIButton) {
         
-        // Получаем текст логина
-        let login = loginTextField.text!
-        // Получаем текст-пароль
-        let password = passwordTextField.text!
-        
-        // Проверяем, верны ли они
-        if login == "admin" && password == "123456" {
-            print("успешная авторизация")
-        } else {
-            print("неуспешная авторизация")
-        }
+//        // Получаем текст логина
+//        let login = loginTextField.text!
+//        // Получаем текст-пароль
+//        let password = passwordTextField.text!
+//
+//        // Проверяем, верны ли они
+//        if login == "admin" && password == "123456" {
+//            print("успешная авторизация")
+//        } else {
+//            print("неуспешная авторизация")
+//        }
         
     }
     
@@ -87,6 +87,43 @@ class LoginFormController: UIViewController {
     @objc func hideKeyboard() {
         self.scrollView?.endEditing(true)
     }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            // Проверяем данные
+            let checkResult = checkUserData()
+            
+            // Если данные не верны, покажем ошибку
+            if !checkResult {
+                showLoginError()
+            }
+            
+            // Вернем результат
+            return checkResult
+        }
+        
+        func checkUserData() -> Bool {
+            guard let login = loginTextField.text,
+                let password = passwordTextField.text else { return false }
+            
+            if login == "a" && password == "1" {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        func showLoginError() {
+            // Создаем контроллер
+            let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+            // Создаем кнопку для UIAlertController
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            // Добавляем кнопку на UIAlertController
+            alter.addAction(action)
+            // Показываем UIAlertController
+            present(alter, animated: true, completion: nil)
+        }
+
+
     
     
     
---
+++ VKClone/Photo/FrendsCell.swift
@@ -0,0 +1,24 @@
+//
+//  FrendsCell.swift
+//  VKClone
+//
+//  Created by Vitaly Prosvetov on 04.08.2020.
+//  Copyright © 2020 Vitaly Prosvetov. All rights reserved.
+//
+
+import UIKit
+
+class FrendsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
